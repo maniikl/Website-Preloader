8a6e3d266ab605246386437961e311eaea9ff862
maniikl <75907376+maniikl@users.noreply.github.com>
2021-03-29 23:38:37 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,16 @@
+// const preLoading = document.getElementsByClassName("loading-section");
+// const loadedContent = document.getElementsByClassName("img-section");
+
+// // document.getElementsByClassName(".img-section").classlist.add.("")
+// window.onload = function(){
+// preLoading.classList.add.("preloading");
+// }
+
+// window.onload = function () {
+//   document.getElementById("hidden").classList.add("prelaoding");
+//   document.getElementById("show").classList.remove("loaded-section");
+// };
+window.onload = function () {
+  document.getElementById("hidden").style.display = "none";
+  document.getElementById("show").style.display = "block";
+};
